1d5bd98c14ea6ad71eb8a5d4d3630b6e542cd810
MxRight <88425424+MxRight@users.noreply.github.com>
2022-09-06 15:15:51 +0300

    Update A.py

--- tests/A/A.py
+++ tests/A/A.py
@@ -1,1 +1,22 @@
-#
+# неэффективный алгоритм, доработать
+A = input()
+B = input()
+dict_of_letter = {i: A.count(i) for i in A}
+result = ['I' for i in range(len(A))]
+
+def plagiator2000(A, B, value='first'):
+    for num, symbol in enumerate(B):
+        if symbol == A[num]:
+            if value == 'first':
+                result[num] = 'P'
+                dict_of_letter[symbol] -= 1
+        else:
+            if value=='second':
+                if A.find(symbol)>=0 and dict_of_letter[symbol] > 0:
+                    result[num] = 'S'
+                    dict_of_letter[symbol] -= 1
+    if value=='first':
+        return plagiator2000(A, B, value='second')
+
+plagiator2000(A, B)
+print(''.join(result))
