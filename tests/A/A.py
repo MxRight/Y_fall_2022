f6317b13765972ed8d7ec96873450a6c1cb92420
Maxim N <88425424+MxRight@users.noreply.github.com>
2022-10-12 18:47:16 +0300

    Update A.py

--- tests/A/A.py
+++ tests/A/A.py
@@ -1,41 +1,32 @@
-# неоптимальный алгоритм, доработать
-A = input()
-B = input()
-dict_of_letter = {i: A.count(i) for i in A}
-result = ['I' for i in range(len(A))]
+from time import perf_counter
 
-def plagiator2000(A, B, value='first'):
+def main(A, B):
+    dict_of_letter = {i: A.count(i) for i in A}
+    result = ['I' for i in range(len(A))]
     for num, symbol in enumerate(B):
         if symbol == A[num]:
-            if value == 'first':
-                result[num] = 'P'
-                dict_of_letter[symbol] -= 1
-        else:
-            if value=='second':
-                if A.find(symbol)>=0 and dict_of_letter[symbol] > 0:
-                    result[num] = 'S'
-                    dict_of_letter[symbol] -= 1
-    if value=='first':
-        return plagiator2000(A, B, value='second')
-
-plagiator2000(A, B)
-print(''.join(result))
-
-"""
-A = input()
-B = input()
-dict_of_letter = {i: A.count(i) for i in A}
-result = ['I' for i in range(len(A))]
-
-for num, symbol in enumerate(B):
-    if symbol == A[num]:
             result[num] = 'P'
             dict_of_letter[symbol] -= 1
+    for num, symbol in enumerate(B):
+        if symbol != A[num]:
+            if symbol not in dict_of_letter or dict_of_letter[symbol] == 0:
+                continue
+            else:
+                if A.find(symbol) >= 0:
+                    result[num] = 'S'
+                    dict_of_letter[symbol] -= 1
+    return ''.join(result)
 
-for num, symbol in enumerate(B):
-    if symbol != A[num]:
-        if A.find(symbol) >= 0 and dict_of_letter[symbol] > 0:
-            result[num] = 'S'
-            dict_of_letter[symbol] -= 1
-print(''.join(result))
-"""
+
+def tests():
+    assert main('CLOUD', 'CUPID') == 'PSIIP', 'PSIIP !'
+    assert main('ALICE', 'ELIBO') == 'SPPII', 'SPPII !'
+    assert main('ABCBCYA', 'ZBBACAA') == 'IPSSPIP', 'IPSSPIP !'
+
+
+if __name__ == "__main__":
+    # tests()
+    #start = perf_counter()   #  засекаем время начала работы функции
+    main(input(), input())
+    #finish = perf_counter()  #  засекаем окончание работы функции
+    #print(finish - start)
